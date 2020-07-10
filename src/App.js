cba15b4ea20a2807c092d6f8e36ccca509a1cb8c
venturads <albert@venturads.com>
2020-07-09 23:17:38 -0700

    fix okta secure routes

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
 
+function onAuthRequired({history}) {
+  history.push('/login');
+}
+
 function App() {
   return (
     <Router>
       <Security
-          issuer="https://dev-409495.oktapreview.com/oauth2/default"
-          client_id="0oafhkg1yupTnPW9z0h7"
+          issuer="https://dev-768648.oktapreview.com/oauth2/default"
+          client_id="0oajyqkdmKMiXIuHg4x6"
           redirect_uri={window.location.origin + '/implicit/callback'}
           onAuthRequired={onAuthRequired}
         >
@@ -20,7 +24,14 @@ function App() {
           <Navbar />
             <div className="container">
               <Route path="/" exact={true} component={Home} />
-              <Route path="/staff" exact={true} component={Staff} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-768648.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
             </div>
         </div>
       </Security>
