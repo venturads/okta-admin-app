2c0de96176b48c6d596ed27008290cc75d315711
venturads <albert@venturads.com>
2020-07-09 23:11:22 -0700

    add security okta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -9,13 +10,20 @@ import Staff from './components/pages/Staff';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-          <div className="container">
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/staff" exact={true} component={Staff} />
-          </div>
-      </div>
+      <Security
+          issuer="https://dev-409495.oktapreview.com/oauth2/default"
+          client_id="0oafhkg1yupTnPW9z0h7"
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
+        >
+        <div className="App">
+          <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/staff" exact={true} component={Staff} />
+            </div>
+        </div>
+      </Security>
     </Router>
   );
 }
