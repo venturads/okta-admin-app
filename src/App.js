2c04f7817171460d82e973a39fe14f5d5bae4524
venturads <albert@venturads.com>
2020-07-09 23:04:03 -0700

    create staff and home js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <h1>hello</h1>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/staff" exact={true} component={Staff} />
       </div>
     </Router>
   );
