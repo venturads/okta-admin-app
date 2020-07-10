5fec733af9e06ea1af5a98363ab7d44ae05ddfa9
venturads <albert@venturads.com>
2020-07-09 23:24:56 -0700

    added sigin okta widget

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
@@ -6,37 +6,40 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 
 function onAuthRequired({history}) {
   history.push('/login');
 }
 
-function App() {
-  return (
-    <Router>
-      <Security
-          issuer="https://dev-768648.oktapreview.com/oauth2/default"
-          client_id="0oajyqkdmKMiXIuHg4x6"
-          redirect_uri={window.location.origin + '/implicit/callback'}
-          onAuthRequired={onAuthRequired}
-        >
-        <div className="App">
-          <Navbar />
-            <div className="container">
-              <Route path="/" exact={true} component={Home} />
-              <SecureRoute path="/staff" exact={true} component={Staff} />
-              <Route
-                path="/login"
-                render={() => (
-                  <Login baseUrl="https://dev-768648.oktapreview.com" />
-                )}
-              />
-              <Route path="/implicit/callback" component={ImplicitCallback} />
-            </div>
-        </div>
-      </Security>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+            issuer="https://dev-768648.oktapreview.com/oauth2/default"
+            client_id="0oajyqkdmKMiXIuHg4x6"
+            redirect_uri={window.location.origin + '/implicit/callback'}
+            onAuthRequired={onAuthRequired}
+          >
+          <div className="App">
+            <Navbar />
+              <div className="container">
+                <Route path="/" exact={true} component={Home} />
+                <SecureRoute path="/staff" exact={true} component={Staff} />
+                <Route
+                  path="/login"
+                  render={() => (
+                    <Login baseUrl="https://dev-768648.oktapreview.com" />
+                  )}
+                />
+                <Route path="/implicit/callback" component={ImplicitCallback} />
+              </div>
+          </div>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
